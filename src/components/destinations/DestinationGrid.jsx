d53ade3e8fccedc41b97784697ec76a4e56fcfef
Glory <akpadiahaglory@gmail.com>
2025-12-16 15:02:21 +0100

    correction

--- src/components/destinations/DestinationGrid.jsx
+++ src/components/destinations/DestinationGrid.jsx
@@ -1,5 +1,5 @@
 
-import santorini from "../../assets/Santorini.jpg";
+import santorini from "../../assets/santorini.jpg";
 import kyoto from "../../assets/kyoto.jpg";
 import bali from "../../assets/bali.jpg";
 import cappadocia from "../../assets/cappadocia.jpg";
